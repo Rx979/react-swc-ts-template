6c48ad1831aab3ba3f4c098361dbab8726664f1e
Rxx <handsomeRxxx@163.com>
2024-10-18 16:48:52 +0800

    chore: update vite configuration and git ignore rules

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,28 @@ export default defineConfig({
         api: 'modern-compiler'
       }
     }
+  },
+  build: {
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[2].split('/')[0];
+          } else if (id.includes('/src/pages/')) {
+            // 针对 pages 目录，按页面拆包
+            const pageName = id.split('/src/pages/')[1].split('/')[0];
+            return `page-${pageName}`;
+          }
+          return 'app'; // 默认应用代码打包在一起
+        },
+        // 定义入口文件的命名规则
+        entryFileNames: 'js/[name].[hash].js',
+        // 定义代码分片文件的命名规则
+        chunkFileNames: 'js/[name].[hash].js',
+        // 定义静态资源文件的命名规则
+        assetFileNames: '[ext]/[name].[hash].[ext]',
+      }
+    }
   }
 });
